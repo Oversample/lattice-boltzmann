58821c51c0c385149608858fde83916c07189a2f
Kevin <kevin2012uk@gmail.com>
2024-05-23 10:28:33 +0100

    added option to save images, changed cylinder to black

--- simulation.cpp
+++ simulation.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <random>
 #include <opencv2/opencv.hpp>
+#include <filesystem>
 
 using namespace std;
 using namespace cv;
@@ -11,10 +12,12 @@ using namespace cv;
 const int Nx = 400;                 //X-resolution
 const int Ny = 100;                 //Y-resolution
 const double rho0 = 40.0;           //Avg Density
-const double tau = 0.6;            //Collision Timescale
+const double tau = 0.8;             //Collision Timescale
 const int Nt = 4000;                //Number of Timestep Interations
+
 const bool plotRealTime = true;     //Display vis
-const int visT = 1;                //Timestep per vis
+const int visT = 10;                 //Timestep per vis
+const bool saveVis = true;
 
 void initialize(vector<vector<vector<double>>> &F, vector<double> &cxs, vector<double> &cys, vector<double> &weights) {
     random_device rd;
@@ -23,11 +26,13 @@ void initialize(vector<vector<vector<double>>> &F, vector<double> &cxs, vector<d
 
     F.resize(Ny, vector<vector<double>>(Nx, vector<double>(9, rho0 / 9.0)));
 
-    for (int y = 0; y < Ny; ++y) {
+    for (int y = 0; y < Ny; ++y) {                  // Loop through each lattice point
         for (int x = 0; x < Nx; ++x) {
-            for (int i = 0; i < 9; ++i) {
-                F[y][x][i] += d(gen);
+            for (int i = 0; i < 9; ++i) {           // Loop through each velocity direction
+                F[y][x][i] += d(gen);            // Add random perturbation to each distribution function
             }
+            // Add a perturbation to the third distribution function (i=3)
+            // based on a cosine function of x position to create initial flow
             F[y][x][3] += 2 * (1 + 0.2 * cos(2 * M_PI * x / Nx * 4));
         }
     }
@@ -42,10 +47,12 @@ void calculateRhoUxUy(const vector<vector<vector<double>>> &F, vector<vector<dou
             double sum_ux = 0.0;
             double sum_uy = 0.0;
             for (int i = 0; i < 9; ++i) {
-                sum_rho += F[y][x][i];
-                sum_ux += F[y][x][i] * cxs[i];
-                sum_uy += F[y][x][i] * cys[i];
+                sum_rho += F[y][x][i];              //Summing all distribution functions at the lattice point
+                sum_ux += F[y][x][i] * cxs[i];      //Summing the product of distribution function and velocity in x-direction
+                sum_uy += F[y][x][i] * cys[i];      //Summing the product of distribution function and velocity in y-direction
             }
+
+            //rho, ux, and uy at the lattice point
             rho[y][x] = sum_rho;
             ux[y][x] = sum_ux / sum_rho;
             uy[y][x] = sum_uy / sum_rho;
@@ -56,13 +63,15 @@ void calculateRhoUxUy(const vector<vector<vector<double>>> &F, vector<vector<dou
 void applyCollision(vector<vector<vector<double>>> &F, const vector<vector<double>> &rho, const vector<vector<double>> &ux,
     const vector<vector<double>> &uy, const vector<double> &cxs, const vector<double> &cys, const vector<double> &weights) {
 
-    for (int y = 0; y < Ny; ++y) {
+    for (int y = 0; y < Ny; ++y) {                                                          // Loop through each lattice point
         for (int x = 0; x < Nx; ++x) {
-            for (int i = 0; i < 9; ++i) {
-                double cu = 3 * (cxs[i] * ux[y][x] + cys[i] * uy[y][x]);
+            for (int i = 0; i < 9; ++i) {                                                   // Loop through each velocity direction
+                double cu = 3 * (cxs[i] * ux[y][x] + cys[i] * uy[y][x]);                    // Calculate the dot product of velocity and lattice direction
                 double cu2 = cu * cu;
                 double u2 = ux[y][x] * ux[y][x] + uy[y][x] * uy[y][x];
-                double feq = rho[y][x] * weights[i] * (1 + cu + 0.5 * cu2 - 1.5 * u2);
+                double feq = rho[y][x] * weights[i] * (1 + cu + 0.5 * cu2 - 1.5 * u2);      // Calculate equilibrium distribution
+                // Collision step: relax distribution function towards equilibrium
+                // with relaxation parameter tau
                 F[y][x][i] += -(1.0 / tau) * (F[y][x][i] - feq);
             }
         }
@@ -73,12 +82,15 @@ void stream(vector<vector<vector<double>>> &F, const vector<double> &cxs, const 
     vector<vector<vector<double>>> F_temp = F;
 
     for (int i = 0; i < 9; ++i) {
+        // Extract the x and y components of the velocity direction
         int cx = static_cast<int>(cxs[i]);
         int cy = static_cast<int>(cys[i]);
         for (int y = 0; y < Ny; ++y) {
             for (int x = 0; x < Nx; ++x) {
+                // Calculate the new coordinates after streaming
                 int newX = (x + cx + Nx) % Nx;
                 int newY = (y + cy + Ny) % Ny;
+                // Stream the distribution function from the temporary array to the new location in F
                 F[newY][newX][i] = F_temp[y][x][i];
             }
         }
@@ -88,11 +100,16 @@ void stream(vector<vector<vector<double>>> &F, const vector<double> &cxs, const 
 void setReflectiveBoundaries(vector<vector<vector<double>>> &F, const vector<vector<bool>> &cylinder) {
     for (int y = 0; y < Ny; ++y) {
         for (int x = 0; x < Nx; ++x) {
+            // Check if the current lattice point is inside the cylinder boundary
             if (cylinder[y][x]) {
                 vector<double> bndryF(9);
+
+                // Copy the distribution functions at the current lattice point to the temporary array
                 for (int i = 0; i < 9; ++i) {
                     bndryF[i] = F[y][x][i];
                 }
+
+                // Set the distribution functions at the boundary based on the reflective boundary conditions
                 F[y][x][1] = bndryF[5];
                 F[y][x][2] = bndryF[6];
                 F[y][x][3] = bndryF[7];
@@ -106,12 +123,16 @@ void setReflectiveBoundaries(vector<vector<vector<double>>> &F, const vector<vec
     }
 }
 
-void visualize(const vector<vector<double>> &ux, const vector<vector<double>> &uy, const vector<vector<bool>> &cylinder) {
-    Mat vorticityImg(Ny, Nx, CV_64F);
+void visualize(const vector<vector<double>> &ux, const vector<vector<double>> &uy, const vector<vector<bool>> &cylinder, int timestep, bool saveT) {
+    Mat vorticityImg(Ny, Nx, CV_64F);       //matrix to store the vorticity image
+
+    // Calculate vorticity at each lattice point and store it in the vorticity image matrix
     for (int y = 0; y < Ny; ++y) {
         for (int x = 0; x < Nx; ++x) {
+            // Calculate vorticity as the curl of the velocity field
             double vorticity = (ux[(y+1) % Ny][x] - ux[(y-1+Ny) % Ny][x]) - (uy[y][(x+1) % Nx] - uy[y][(x-1+Nx) % Nx]);
             if (cylinder[y][x]) {
+                // Vorticity value to NaN if the lattice point is inside the boundary
                 vorticityImg.at<double>(y, x) = NAN;
             } else {
                 vorticityImg.at<double>(y, x) = vorticity;
@@ -119,42 +140,65 @@ void visualize(const vector<vector<double>> &ux, const vector<vector<double>> &u
         }
     }
 
-    // Normalize and convert vorticity image to CV_8UC1
+    //Normalize and convert vorticity image to CV_8UC1
     Mat normalizedVorticityImg;
     normalize(vorticityImg, normalizedVorticityImg, 0, 1, NORM_MINMAX);
     normalizedVorticityImg *= 255;
     normalizedVorticityImg.convertTo(normalizedVorticityImg, CV_8UC1);
 
-    // Apply color map
+    //Apply color map
     Mat colorMappedImg;
     applyColorMap(normalizedVorticityImg, colorMappedImg, COLORMAP_JET);
 
-    // Set cylinder area to white
+    //Set cylinder area to black
     for (int y = 0; y < Ny; ++y) {
         for (int x = 0; x < Nx; ++x) {
             if (cylinder[y][x]) {
-                colorMappedImg.at<Vec3b>(y, x) = Vec3b(255, 255, 255);
+                colorMappedImg.at<Vec3b>(y, x) = Vec3b(0, 0, 0);
             }
         }
     }
 
-    // Display the image
+    //Display the image
     imshow("Vorticity", colorMappedImg);
+
+    //Save Images
+    if (saveT) {
+        string folderName = "img";
+        filesystem::create_directories(folderName);
+
+        string filename = folderName + "/vorticity_" + to_string(timestep) + ".png";
+        imwrite(filename, colorMappedImg);
+    }
+
     waitKey(1);
 }
 
 int main() {
+    //Clear saved Images
+    string folderName = "img";
+
+    for (const auto& entry : filesystem::directory_iterator(folderName)) {
+        filesystem::remove(entry.path());
+    }
+
+
+    filesystem::create_directories(folderName);
+    //Lattice velocities and weights
     vector<double> cxs = {0, 0, 1, 1, 1, 0, -1, -1, -1};
     vector<double> cys = {0, 1, 1, 0, -1, -1, -1, 0, 1};
     vector<double> weights = {4.0 / 9, 1.0 / 9, 1.0 / 36, 1.0 / 9, 1.0 / 36, 1.0 / 9, 1.0 / 36, 1.0 / 9, 1.0 / 36};
 
+    // Lattice distribution function F
     vector<vector<vector<double>>> F;
     initialize(F, cxs, cys, weights);
 
+    //Arrays to store density (rho) and velocity components (ux, uy)
     vector<vector<double>> rho(Ny, vector<double>(Nx));
     vector<vector<double>> ux(Ny, vector<double>(Nx));
     vector<vector<double>> uy(Ny, vector<double>(Nx));
 
+    //Cylinder boundary
     vector<vector<bool>> cylinder(Ny, vector<bool>(Nx, false));
     for (int y = 0; y < Ny; ++y) {
         for (int x = 0; x < Nx; ++x) {
@@ -163,7 +207,7 @@ int main() {
             }
         }
     }
-
+    //Main loop - simulation iterations
     for (int it = 0; it < Nt; ++it) {
         cout << "Iteration: " << it << endl;
 
@@ -176,7 +220,7 @@ int main() {
         applyCollision(F, rho, ux, uy, cxs, cys, weights);
 
         if (plotRealTime && (it % visT == 0 || it == Nt - 1)) {
-            visualize(ux, uy, cylinder);
+            visualize(ux, uy, cylinder, it, saveVis);
         }
     }
 
